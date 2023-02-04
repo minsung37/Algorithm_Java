ae0cab0a0119c84f721b45778b97dcf31f6dbd88
minsung37 <tampang99@naver.com>
2023-02-04 15:09:39 +0900

    S_제로, S_카드2

---
+++ beakjoon/basic/제로.java
@@ -0,0 +1,26 @@
+package algorithm.beakjoon.basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class 제로 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < n; i++){
+            int number = Integer.parseInt(br.readLine());
+            if (number != 0) {
+                stack.add(number);
+            } else {
+                stack.pop();
+            }
+        }
+        int result = 0;
+        for (int i : stack)
+            result = result + i;
+        System.out.println(result);
+    }
+}
