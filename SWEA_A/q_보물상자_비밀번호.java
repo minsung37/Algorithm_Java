7059c4e71b65f8345d3015ce56187acb1f02fe9d
minsung37 <tampang99@naver.com>
2023-03-22 22:54:35 +0900

    A_보물상자 비밀번호

--- SWEA_A/q_보물상자_비밀번호.java
+++ SWEA_A/q_보물상자_비밀번호.java
@@ -1,2 +1,25 @@
-package algorithm.A;public class q_보물상자_비밀번호 {
+package algorithm.SWEA_A;
+
+import java.util.Scanner;
+
+public class q_보물상자_비밀번호 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 0; t < T; t++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (m % 2 == 1)
+                    count++;
+                m = m / 2;
+            }
+            if (count == n) {
+                System.out.printf("#%d ON\n", t + 1);
+            } else {
+                System.out.printf("#%d OFF\n", t + 1);
+            }
+        }
+    }
 }
